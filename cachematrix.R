b5754c1dbd85f80a466823225e6ea1d427485e2f
BryceThorrold <b.thorrold@mweb.co.za>
2020-07-28 11:55:52 +0200

    Added All my code for Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
+## This function will create the vector the first time, put the list of functions
+## into x kist and 'publish'x outside of the immediate environment
+## You can use $set afterwards to change the original data set
+## Note you have to run it the first time and place the result into 'x'
 
 
-## Write a short comment describing this function
+
+makeCacheMatrix <- function(current = matrix()) {
+    inverse_matrix <- NULL
+    set <- function(new) {
+      current <<- new
+      inverse_matrix <<- NULL
+    }
+    get <- function() current
+    setinverse <- function(inverted) inverse_matrix <<- inverted
+    getinverse <- function() inverse_matrix
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }
+
+  
+## We then use cachesolve to solve for the matrix passed to it, called x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  exists <- x$getinverse()
+  if(!is.null(exists)) {
+    message("getting cached data")
+    return(exists)
+  }
+  data <- x$get()
+  inverted <- solve(data, ...)
+  x$setinverse(inverted)
+  inverted
 }
